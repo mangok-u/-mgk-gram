f9f3fa430fa409c896194e21812f340e7b03197e
goku-naoki <naoki.mangoku@di-v.co.jp>
2020-08-16 16:56:48 +0900

    Update hello_vue.js

--- app/javascript/packs/hello_vue.js
+++ app/javascript/packs/hello_vue.js
@@ -9,10 +9,10 @@ import Vue from 'vue'
 import App from '../app.vue'
 import router from '../router.js'
 import firebase from 'firebase'
-import store from './store';
+import store from '../store';
 import "firebase/auth";
 import '../plugins'
-import store from '../store'
+
  //普通にnpminstall de行けた
 const config = {
   apiKey: "AIzaSyDxmb4KQEh-naza2Dt-a48_jFwGf6UQDg8",
