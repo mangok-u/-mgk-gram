1805dad8c0b6f90795d3b6b3b0fe25d6cbecd6aa
goku-naoki <naoki.mangoku@di-v.co.jp>
2020-08-23 22:48:13 +0900

    follow数をuserに反映

--- app/controllers/api/v1/follows_controller.rb
+++ app/controllers/api/v1/follows_controller.rb
@@ -2,7 +2,12 @@ class Api::V1::FollowsController < ApiController
 
   def create
     @follwe=Follow.new(follow_params)
+    following=User.find(follow_params[:following_id])
+    follower=User.find(follow_params[:follower_id])
     if @follwe.save
+      following.increment!(:following, 1)
+      follower.increment!(:follower, 1)
+
       render json: @follwe, staus: :created
     else
       render json: { errors: @follwe.errors.full_messages }, status: :unprocessable_entity
