1b6987e3eebd188af08e49017b1e2879b7e4f6fd
goku-naoki <naoki.mangoku@di-v.co.jp>
2020-09-13 18:08:40 +0900

    Update webpacker.rb

--- config/initializers/webpacker.rb
+++ config/initializers/webpacker.rb
@@ -5,3 +5,4 @@ Webpacker::Compiler.env["PROJECTID"] =  Rails.application.credentials.project_id
 Webpacker::Compiler.env["STORAGEBUCKET"] =  Rails.application.credentials.storage_bucket
 Webpacker::Compiler.env["MESSAGINGSENDERID"] =  Rails.application.credentials.messaging_sender_id
 Webpacker::Compiler.env["APPID"] =  Rails.application.credentials.app_id
+
