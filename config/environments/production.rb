cd51cd53642403d6d2ed77e475a0d1e98053bd67
goku-naoki <naoki.mangoku@di-v.co.jp>
2020-09-14 15:31:31 +0900

    Update production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -10,7 +10,7 @@ Rails.application.configure do
   # Rake tasks automatically ignore this option for performance.
   config.eager_load = true
 
-  host = 'mgk-gram.herokuapp'
+  host = 'mgk-gram.herokuapp.com'
   Rails.application.routes.default_url_options[:host] = host
   # Full error reports are disabled and caching is turned on.
   config.consider_all_requests_local       = false
